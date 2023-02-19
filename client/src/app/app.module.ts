56eab19138508ec72d1f18d1fc58ab4969c4d263
roy brand <roybran@gmail.com>
2023-02-19 07:33:30 +0200

    persist login

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
+import {BsDropdownModule } from 'ngx-bootstrap/dropdown'
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule 
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    BsDropdownModule.forRoot(),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
